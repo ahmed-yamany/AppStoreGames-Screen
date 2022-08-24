e9231479eaedc7cd80f64938194c1f0145269c6b
Ahmed Yamany <ahmed.mohammed.yamany@gmail.com>
2022-08-24 18:16:06 +0200

    create promoted section

--- AppStoreLayout/ViewController.swift
+++ AppStoreLayout/ViewController.swift
@@ -20,17 +20,71 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // MARK: Collection View Setup
-        collectionView.collectionViewLayout = createLayout()
-
+        collectionView.collectionViewLayout = createSectionLayout()
+        // To display promoted items, you'll need to register PromotedAppCollectionViewCell with the collection view and then configure the data source to return that cell for items in the section.”
+        collectionView.register(PromotedAppCollectionViewCell.self, forCellWithReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier)
         configureDataSource()
     }
     
-    func createLayout() -> UICollectionViewLayout {
-        fatalError("TODO: Create Layout")
+    func createSectionLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let section = self.sections[sectionIndex]
+            switch section{
+            case .promoted:
+                // MARK: - Promoted Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(300))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                
+                let section  = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                
+                return section
+            default:
+                return nil
+            }
+            
+        }
+        
+        return layout
     }
 
     func configureDataSource() {
-        fatalError("TODO: Create Data Source")
+        // MARK: Data Source initialization
+        dataSource = .init(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
+            let section = self.sections[indexPath.section]
+            switch section{
+                
+            case .promoted:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier, for: indexPath) as! PromotedAppCollectionViewCell
+                
+                if let item = itemIdentifier.app{
+                    cell.configureCell(item)
+                }
+                return cell
+                
+            default:
+                fatalError("Not yet implemented")
+                
+            }
+        }
+        )
+        
+        // MARK: SnapShot Definition
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        // append promoted section and it's items
+        snapshot.appendSections([.promoted])
+        snapshot.appendItems(Item.promotedApps, toSection: .promoted)
+        
+        
+        sections = snapshot.sectionIdentifiers
+        dataSource.apply(snapshot)
+
+
     }
 }
 
