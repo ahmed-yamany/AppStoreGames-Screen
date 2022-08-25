2a8271d8c0a324174f7b1978afd23e770e33d35c
Ahmed Yamany <ahmed.mohammed.yamany@gmail.com>
2022-08-25 15:09:51 +0200

    add supplementary view to sections

--- AppStoreLayout/ViewController.swift
+++ AppStoreLayout/ViewController.swift
@@ -2,36 +2,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-        
+    // MARK: IBOutlets
+    @IBOutlet var collectionView: UICollectionView!
+    
     // MARK: Section Definitions
     enum Section: Hashable {
         case promoted
         case standard(String)
         case categories
     }
-
-    @IBOutlet var collectionView: UICollectionView!
     
+    // MARK: Properties
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    
     var sections = [Section]()
     
+    enum SupplementaryViewKind {
+        static let header = "header"
+        static let topLine = "topLine"
+        static let bottomLine = "bottomLine"
+    }
+    
+    // MARK: - Views
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // MARK: Collection View Setup
+        
         collectionView.collectionViewLayout = createSectionsLayout()
         // To display promoted items, you'll need to register PromotedAppCollectionViewCell with the collection view and then configure the data source to return that cell for items in the section.”
         collectionView.register(PromotedAppCollectionViewCell.self, forCellWithReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier)
         collectionView.register(StandardAppCollectionViewCell.self, forCellWithReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier)
         collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier)
+        // registere supplementary Views
+        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: SupplementaryViewKind.header, withReuseIdentifier: SectionHeaderView.reuseIdentifier)
+        collectionView.register(LineView.self, forSupplementaryViewOfKind: SupplementaryViewKind.topLine, withReuseIdentifier: LineView.reuseIdentifier)
+        collectionView.register(LineView.self, forSupplementaryViewOfKind: SupplementaryViewKind.bottomLine, withReuseIdentifier: LineView.reuseIdentifier)
         
         configureDataSource()
     }
     
     func createSectionsLayout() -> UICollectionViewLayout {
+        // MARK: - Sections Layout
         let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-            
             let section = self.sections[sectionIndex]
             switch section{
             case .promoted:
@@ -46,6 +58,15 @@ class ViewController: UIViewController {
                 let section  = NSCollectionLayoutSection(group: group)
                 section.orthogonalScrollingBehavior = .groupPagingCentered
                 
+                let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(44))
+                let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: SupplementaryViewKind.topLine, alignment: .top)
+                headerItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                
+                section.boundarySupplementaryItems = [headerItem]
+
+                
+               
+                
                 return section
             case .standard:
                 // MARK: - Standard Section Layout
@@ -57,7 +78,13 @@ class ViewController: UIViewController {
                 group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
                 let section = NSCollectionLayoutSection(group: group)
                 section.orthogonalScrollingBehavior = .groupPagingCentered
+                let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(44))
                 
+                let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: SupplementaryViewKind.header, alignment: .top)
+                headerItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                
+                section.boundarySupplementaryItems = [headerItem]
+
                 return section
             case .categories:
                 // MARK: - Categories Section Layout
@@ -83,13 +110,37 @@ class ViewController: UIViewController {
             
             }
             
+            
         }
         
         return layout
     }
-
+    
+    // MARK: Configure Data Source
     func configureDataSource() {
-        // MARK: Data Source initialization
+        dataSourceInitialization()
+        
+        // MARK: SnapShot Definition
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        // append promoted section and it's items
+        let popularSection = Section.standard("Popular this week")
+        let essentialSection = Section.standard("Essential picks")
+        
+        snapshot.appendSections([.promoted, popularSection, essentialSection, .categories])
+        snapshot.appendItems(Item.promotedApps, toSection: .promoted)
+        snapshot.appendItems(Item.popularApps, toSection: popularSection)
+        snapshot.appendItems(Item.essentialApps, toSection: essentialSection)
+        snapshot.appendItems(Item.categories, toSection: .categories)
+        
+        
+        sections = snapshot.sectionIdentifiers
+        dataSource.apply(snapshot)
+
+
+    }
+    
+    // MARK: Data Source initialization
+    func dataSourceInitialization(){
         dataSource = .init(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
             let section = self.sections[indexPath.section]
             switch section{
@@ -120,26 +171,38 @@ class ViewController: UIViewController {
                 return cell
             
             }
-        }
-        )
+        })
         
-        // MARK: SnapShot Definition
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        // append promoted section and it's items
-        let popularSection = Section.standard("Popular this week")
-        let essentialSection = Section.standard("Essential picks")
+        supplementaryViewProfider()
         
-        snapshot.appendSections([.promoted, popularSection, essentialSection, .categories])
-        snapshot.appendItems(Item.promotedApps, toSection: .promoted)
-        snapshot.appendItems(Item.popularApps, toSection: popularSection)
-        snapshot.appendItems(Item.essentialApps, toSection: essentialSection)
-        snapshot.appendItems(Item.categories, toSection: .categories)
-        
-        
-        sections = snapshot.sectionIdentifiers
-        dataSource.apply(snapshot)
-
-
     }
+    
+    // MARK: Supplementary View Provider”
+    func supplementaryViewProfider(){
+        dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) -> UICollectionReusableView? in
+            let section = self.sections[indexPath.section]
+            switch section {
+            case .promoted:
+                let lineView = collectionView.dequeueReusableSupplementaryView(ofKind: SupplementaryViewKind.topLine, withReuseIdentifier: LineView.reuseIdentifier, for: indexPath) as! LineView
+                
+                return lineView
+            case .standard(let name):
+                return self.vendsSectionsHeader(collectionView: collectionView, name: name, indexPath: indexPath)
+            case .categories:
+                return self.vendsSectionsHeader(collectionView: collectionView, name: "Top Categories", indexPath: indexPath)
+            }
+            
+            
+        }
+    }
+    func vendsSectionsHeader(collectionView: UICollectionView, name:String, indexPath: IndexPath) -> UICollectionReusableView{
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: SupplementaryViewKind.header, withReuseIdentifier: SectionHeaderView.reuseIdentifier, for: indexPath) as! SectionHeaderView
+    
+        headerView.setTitle(name)
+        return headerView
+    }
+    
 }
+
+
 
